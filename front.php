7d8c4b1768bb2601c74d04b7ea8ff9732c321a3b
paulsboutique <barnard704@gmail.com>
2021-04-08 13:11:39 +0930

    commit on 'live'

--- front.php
+++ front.php
@@ -12,11 +12,11 @@ p {
 }
 
 h1{
-    font-size:40px;
+    font-size:20px;
 }
 
 h2{
-    font-size:160px;
+    font-size:120px;
 }
 
 </style>
@@ -48,13 +48,12 @@ $ttw = $conn->query($sql);
 
 
 echo '
-<table style="height: 99%" width="99%">
+<table style="height: 600" width="auto">
 <tbody>
-<tr style="height: 25%; background-color: #6699cc;">
-<td style="width: 33%; height: 25%;">&nbsp;<img src="http://localhost/house/seaton.png" alt="" /></td>
-<td style="width: 33%; height: 25%;">&nbsp;</td>
-<td style="width: 33%; height: 25%;"><h1 style="text-align: center;"><span style="color: #ffffff;"><strong>2021 HOUSE SHIELD</strong></span></h1>
-<h1 style="text-align: center;"><span style="color: #ffffff;"><strong>LEADER BOARD</strong></h1></td>
+<tr style="height: 15%; background-color: #6699cc;">
+<td style="width: 33%; height: 15%;"><h1 style="text-align: center;"><span style="color: #ffffff;"><strong>2021 HOUSE SHIELD</strong></span></h1></td>
+<td style="width: 33%; height: 15%;">&nbsp;</td>
+<td style="width: 33%; height: 15%;"><h1 style="text-align: center;"><span style="color: #ffffff;"><strong>LEADER BOARD</strong></h1></td>
 </tr>
 <tr >
 <td style="width: 33%; background-color: orange; height: 25%;"><p style="text-align: center;"><span style="color: #ffffff;"><strong>ORANGE</strong</p></td>
@@ -62,21 +61,21 @@ echo '
 <td style="width: 33%; height: 25%; background-color: #003366;"><p style="text-align: center;"><span style="color: #ffffff;"><strong>BLUE</strong</p></td>
 </tr>
 <tr >
-<td style="width: 33%; background-color: orange; height: 75%;"><h2 style="text-align: center;"><span style="color: #ffffff;"><strong>';
+<td style="width: 33%; background-color: orange; height: 45%;"><h2 style="text-align: center;"><span style="color: #ffffff;"><strong>';
 if ($tto->num_rows > 0) {  //prints term total for orange
     while($row = $tto->fetch_assoc()) {
         echo  $row["termtotal"]; 
     }
 }; 
 echo '</strong></h2></td>
-<td style="width: 33%; height: 75%;"><h2 style="text-align: center; "><span style="color: #000000;"><strong>';
+<td style="width: 33%; height: 45%;"><h2 style="text-align: center; "><span style="color: #000000;"><strong>';
 if ($ttb->num_rows > 0) { //prints term total for white
     while($row = $ttw->fetch_assoc()) {
         echo  $row["termtotal"]; 
     }
 }; 
 echo '</strong></h2></td>
-<td style="width: 33%; height: 75%; background-color: #003366;"><h2 style="text-align: center;"><span style="color: #ffffff;"><strong>'; 
+<td style="width: 33%; height: 45%; background-color: #003366;"><h2 style="text-align: center;"><span style="color: #ffffff;"><strong>'; 
 if ($ttb->num_rows > 0) { //prints term total for blue
     while($row = $ttb->fetch_assoc()) {
         echo  $row["termtotal"]; 
